e2de46ebe0a98f9da0b4ac644127a29a34e8678a
Harikrishnan49 <harikrishnan15me048@gmail.com>
2024-10-03 15:58:46 +0530

    Another Test

---
+++ Trial1/src/org/mani/OpenAmazon.java
@@ -0,0 +1,15 @@
+package org.mani;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenAmazon {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\harik\\eclipse-workspace\\Trial1\\Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+		
+	}
+
+}
